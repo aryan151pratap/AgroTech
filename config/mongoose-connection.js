f786160708e517462a0dc3d24bc8b7f224afc0da
Aryan Pratap <apratapsingh151@gmail.com>
2025-02-12 16:03:23 +0530

    mongo_conn

--- config/mongoose-connection.js
+++ config/mongoose-connection.js
@@ -29,22 +29,4 @@ async function connectDB() {
 
 module.exports = connectDB;
 
-// const mongoose = require("mongoose");
-// const dbgr = require("debug")("development:mongoose");
-// const config = require("config");
 
-// const MONGO_URI = config.get("MONGODB_URI");
-
-// async function connectDB() {
-//     try {
-//         await mongoose.connect(`${MONGO_URI}/otpdb`);
-
-//         console.log("✅ Connected to MongoDB");
-
-//         return mongoose.connection;
-//     } catch (err) {
-//         console.log( err.message);
-//         process.exit(1);
-//     }
-// }
-// module.exports = connectDB();
