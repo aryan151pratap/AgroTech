849bf9f0878a8f64142579e4d4c992b8495fc409
Aryan Pratap <apratapsingh151@gmail.com>
2025-02-14 19:10:23 +0530

    change

--- api/app.js
+++ api/app.js
@@ -1,44 +1,46 @@
 const express = require("express");
-const app = express();
-const path = require('path');
-const cookieParser = require('cookie-parser');
+const serverless = require("serverless-http"); // Required for Vercel
+const path = require("path");
+const cookieParser = require("cookie-parser");
 const flash = require("connect-flash");
 const expressSession = require("express-session");
+const dotenv = require("dotenv");
 
-const userRoute = require("../routes/userRoute");
-const reelRoute = require("../routes/reelRoute");
-const shopRoute = require('../routes/shopRoute');
-const transactionRoute = require("../routes/transactionRoute");
+dotenv.config();
 
+const app = express();
 
-require('dotenv').config();
-
-app.set('view engine', 'ejs');
+// Middleware
+app.set("view engine", "ejs");
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "../public"))); // Adjusted path
 
 app.use(
-	expressSession({
-		resave: false,
-		saveUninitialized: false,
-		secret: process.env.EXPRESS_SESSION_SECRET,
-	})
+  expressSession({
+    resave: false,
+    saveUninitialized: false,
+    secret: process.env.EXPRESS_SESSION_SECRET,
+  })
 );
 
 app.use(flash());
 
+// Database Connection
 const db = require("../config/mongoose-connection");
 
+// Routes
+const userRoute = require("../routes/userRoute");
+const reelRoute = require("../routes/reelRoute");
+const shopRoute = require("../routes/shopRoute");
+const transactionRoute = require("../routes/transactionRoute");
 
-app.use('/users', userRoute);
+app.use("/users", userRoute);
 app.use("/reel", reelRoute);
 app.use("/shop", shopRoute);
 app.use("/tran", transactionRoute);
 
-
-const port = process.env.SERVER_PORT | 3000;
-app.listen(port, '0.0.0.0', function(){
-    console.log("Server listen on port " + port);
-});
+// Export for Vercel Serverless
+module.exports = app;
+module.exports.handler = serverless(app);
